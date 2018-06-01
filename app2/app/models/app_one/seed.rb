89ab24c9c8fa227c7864113d3d03977d6c12afcd
hao <nvhaotl@gmail.com>
2018-06-02 00:40:54 +0700

    get correct group

--- app2/app/models/app_one/seed.rb
+++ app2/app/models/app_one/seed.rb
@@ -1,6 +1,8 @@
 class AppOne::Seed < AppOne::ApiModel
   has_one :fruit
   scope :get_random_for_consumer, ->{
-    where(is_consumed: false).order("RANDOM()").first
+    where(is_consumed: false).
+    where(group: 2).
+    order("RANDOM()").first
   }
 end
