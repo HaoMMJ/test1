682ab270ac7f5464909ae13f5de95511ae0a5994
hao <nvhaotl@gmail.com>
2018-06-01 10:26:46 +0700

    remove unused code

--- app2/app/models/application_record.rb
+++ app2/app/models/application_record.rb
@@ -1,4 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection "#{ENV['DATABASE_URL']}"
 end
--- app2/config/initializers/load_db_config.rb
+++
@@ -1,4 +0,0 @@
-DB_ONE_CONFIG = YAML.load_file("#{Rails.root}/config/db.yml")[Rails.env+'_one']
-
-# As our secondary db is being defined in production_sec.
-DB_TWO_CONFIG = YAML.load_file("#{Rails.root}/config/db.yml")[Rails.env+'_two']
