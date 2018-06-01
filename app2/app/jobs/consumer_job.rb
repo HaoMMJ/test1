e30619fe579db052102869f056c540922c6a83e8
hao <nvhaotl@gmail.com>
2018-06-01 10:43:56 +0700

    Store seed and fruit in app1 db, keep app2 db

--- app2/app/jobs/consumer_job.rb
+++ app2/app/jobs/consumer_job.rb
@@ -6,7 +6,13 @@ class ConsumerJob < ApplicationJob
     if seed.present?
       seed.is_consumed = true
       seed.save
-      Fruit.create(seed_id: seed.id, name: "j-#{rand}")
+      fruit_label = "j-#{rand}"
+      Fruit.create(seed_id: seed.id, name: fruit_label)
+      app_one_seed = AppOne::Seed.where(label: seed.label, group: 2).first
+      app_one_seed = AppOne::Seed.create(label: seed.label, group: 2) if app_one_seed.nil?
+      app_one_seed.is_consumed = true
+      app_one_seed.save
+      AppOne::Fruit.create(seed_id: app_one_seed.id, name: fruit_label)
     end
   end
 end
--- app2/app/jobs/seeder_job.rb
+++ app2/app/jobs/seeder_job.rb
@@ -2,6 +2,8 @@ class SeederJob < ApplicationJob
   queue_as :default
  
   def perform()
-    Seed.create(label: "2-#{rand}")
+    new_label = "2-#{rand}"
+    Seed.create(label: new_label)
+    AppOne::Seed.create(label: new_label, group: 2)
   end
 end
